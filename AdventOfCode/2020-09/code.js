fd81967fa98fb239c20b82303c1ea8f689f95d46
Rob CW <rob@uuthree.com>
2021-02-02 19:10:08 +0000

    Added Day 9 Part 2

--- AdventOfCode/2020-09/code.js
+++ AdventOfCode/2020-09/code.js
@@ -30,8 +30,45 @@ const findPair = (targetValue, values, logData = false) => {
         console.log('----');
     }
 
+    // Return value indicates success/failure of the search
     return result.length > 0;
 }
+
+const findContiguousRange = (targetValue, values) => {
+
+    // Within the passed array of values, find a contiguous sequence
+    // of values that add up to the target value
+
+    let result = [];
+    let currentIndex = 0;
+    let targetValueFound = false;
+    let startIndex = 0;
+    let endIndex = 0;
+
+    for (let i = currentIndex; i < values.length; i++) {
+        startIndex = i;
+        let currentTotal = values[i];
+        for (let n = i + 1; n < values.length; n++) {
+            currentTotal += values[n];
+            if (currentTotal === targetValue) {
+                targetValueFound = true;
+                endIndex = n;
+                break;
+            }
+        }
+        if (targetValueFound) {
+            break;
+        }
+    }
+
+    if (targetValueFound) {
+        return values
+            .slice(startIndex, endIndex + 1)
+            .sort();
+    }
+
+    return [];
+};
 
 const processFile = (rangeLength, startIndex, allValues, logData = false) => {
 
@@ -53,12 +90,21 @@ const processFile = (rangeLength, startIndex, allValues, logData = false) => {
         return valueWithoutAPair;
     }, null);
 
-    console.log(`Value with no pairs that sum to it: ${answer}`);
+    return answer;
 }
 
 // Part One
+const answer1 = processFile(25, 25, data, false);
 console.log('---------------------------------------------------------');
 console.log('Part 1 answer:');
-processFile(25, 25, data, false);
+console.log(`Value with no pairs that sum to it:`);
+console.log(answer1);
 console.log('---------------------------------------------------------');
 
+// Part Two
+const range = findContiguousRange(answer1, data);
+const smallestPlusLargest = range[0] + range[range.length - 1];
+console.log('---------------------------------------------------------');
+console.log('Part 2 answer:');
+console.log(smallestPlusLargest);
+console.log('---------------------------------------------------------');
