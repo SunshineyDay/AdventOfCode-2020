83108013dee692f4c618740c1380f6f3a83aaf84
Rob CW <rob@uuthree.com>
2021-02-02 15:05:09 +0000

    Added logging of data to readFile

--- AdventOfCode/Utils/readFile.js
+++ AdventOfCode/Utils/readFile.js
@@ -1,6 +1,7 @@
+const util = require('util');
 const fs = require("fs");
 
-const readFile = (filePath, splitNewLines = true) => {
+const readFile = (filePath, splitNewLines = true, logDataToConsole = false) => {
 
     // Read the file
     let data;
@@ -17,7 +18,14 @@ const readFile = (filePath, splitNewLines = true) => {
         console.log("");
         console.log(`Reading the file: ${filePath} - ${result.length} rows found`);
         console.log("");
+        if (logDataToConsole) {
+            console.log(util.inspect(result, false, null, true));
+        }
         return result;
+    }
+
+    if (logDataToConsole) {
+        console.log(util.inspect(data, false, null, true));
     }
 
     return data;
